a388758dac606f7130d9d22f136569d042eb83a6
Ángel Pereira <ampr2003@gmail.com>
2022-11-30 10:47:00 +0100

    Cambios

--- cliente.py
+++ cliente.py
@@ -1,7 +1,44 @@
-import socket, os
+import socket, os, random
+
+#Metodo para seleccionar las 5 preguntas de forma aleatoria.
+    
+def selector():
+    archivo = open("preguntas.txt")
+    listaPreguntas = archivo.readlines()
+    listaPreguntadas = []
+    for i in range(5):
+        pregunta = random.choice(listaPreguntas)
+        listaPreguntadas.append(pregunta[0:(len(pregunta)-2)])
+        listaPreguntas.remove(pregunta)
+    return listaPreguntadas
+
+#Método para mostrar al usuario las 5 preguntas correspondientes y comprobar si la respuesta indicada es correcta devolviendo el numero de aciertos.
+
+def preguntas(listaPreguntadas):
+    res = 0
+    for i in range (len(listaPreguntadas)):
+        pregunta = listaPreguntadas[i]
+        print(pregunta[0:(len(pregunta)-2)])
+        option = input("Introducir la opción correcta (1, 2, 3, 4) -> ")
+        if(comprobarRespuesta(option,pregunta)):
+            print("Has introducido la respuesta correcta.")
+            res += 1
+        else:
+            print("La respuesta proporcionada no es correcta.")
+    print("La cantidad de aciertos que has obtenido es de: " + str(res))
+    return res
+#Metodo para comprobar que la respuesta proporcionada es la correcta.
+
+def comprobarRespuesta(option, pregunta):
+    listaPregunta = pregunta.split(";")
+    if(listaPregunta[len(listaPregunta)-1]==option):
+        return True
+    else:
+        return False
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost", 9003))
+s.connect(("10.10.1.245", 9003))
 
 existe = ""
 while(existe != "t"):
@@ -44,3 +81,6 @@ print("Jugador 1: " + str(dicc_jug[0][2]))
 print("Jugador 2: " + str(dicc_jug[1][2]))
 print("Jugador 3: " + str(dicc_jug[2][2]))
 print("Jugador 4: " + str(dicc_jug[3][2]))
+
+puntos = preguntas(selector())
+print("Has conseguido " + str(puntos) + " puntos")
--- servidor.py
+++ servidor.py
@@ -4,7 +4,7 @@ import os, os.path
 from time import sleep
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("localhost", 9003))
+server.bind(("10.10.1.245", 9003))
 server.listen(1)
 nombres = []
 cant = 0
@@ -41,39 +41,3 @@ while True:
         socket_cliente.send(existe.encode())
     nombre = socket_cliente.recv(1024).decode()
     print(nombre)
-        
-    #Metodo para seleccionar las 5 preguntas de forma aleatoria.
-    
-    def selector():
-        archivo = open("preguntas.txt")
-        listaPreguntas = archivo.readlines()
-        listaPreguntadas = []
-        for i in range(5):
-            pregunta = random.choice(listaPreguntas)
-            listaPreguntadas.append(pregunta[0:(len(pregunta)-2)])
-            listaPreguntas.remove(pregunta)
-        return listaPreguntadas
-    
-    #Método para mostrar al usuario las 5 preguntas correspondientes y comprobar si la respuesta indicada es correcta devolviendo el numero de aciertos.
-    
-    def preguntas(listaPreguntadas):
-        res = 0
-        for i in range (len(listaPreguntadas)):
-            pregunta = listaPreguntadas[i]
-            print(pregunta[0:(len(pregunta)-2)])
-            option = input("Introducir la opción correcta (1, 2, 3, 4) -> ")
-            if(comprobarRespuesta(option,pregunta)):
-                print("Has introducido la respuesta correcta.")
-                res += 1
-            else:
-                print("La respuesta proporcionada no es correcta.")
-        print("La cantidad de aciertos que has obtenido es de: " + res)
-        return res
-    #Metodo para comprobar que la respuesta proporcionada es la correcta.
-    
-    def comprobarRespuesta(option, pregunta):
-        listaPregunta = pregunta.split(";")
-        if(listaPregunta[len(listaPregunta)-1]==option):
-            return True
-        else:
-            return False
